048025bc89efea6eeef02096605759451923905a
TomCabral <apbc@cesar.school>
2023-05-17 20:52:33 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+  typedef struct Register{
+    char name[50];
+    char email[50];
+    char *password;
+    long int cpf;
+    int option;
+  }Register;
+  
+  storeRegister(char name[50], char email[50], char *password, long int cpf, FILE Register){
+    
+  }
+
+int main(void) {
+  
+  return 0;
+}
