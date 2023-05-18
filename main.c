d6570c41c8be299c05c8f62f3888e13f5cbaf6c5
TomCabral <apbc@cesar.school>
2023-05-17 22:37:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,63 @@
 #include <string.h>
 #include <stdlib.h>
 
-  typedef struct Register{
+  typedef struct{
     char name[50];
     char email[50];
     char *password;
-    long int cpf;
+    char cpf[14];
     int option;
-  }Register;
-  
-  void storeRegister(char name[50], char email[50], char *password, long int cpf, FILE Register){
+  } register;
+
+  //inserre um novo "perfil" (não sei nome melhor)
+  register* createProfile(char name[50], char email[50], char *password, char cpf[14], int option) {
+    register* ptr = malloc(sizeof(register));
+    if (ptr == NULL) {
+        printf("Erro ao tentar criar um novo perfil.\n");
+        return NULL;
+    }
+    strncpy(ptr->name, name, 50);
+    strncpy(ptr->email, email, 50);
+    ptr->password = strdup(password);
+    strncpy(ptr->cpf, cpf, 14);
+    ptr->option = option;  
+
+    return ptr;
+  }
+
+  //libera a memoria do ponteiro e libera da senha pq strdup() duplica a string
+  void freeProfile(register* ptr) {
+    free(ptr->password);
+    free(ptr);
+  }
+
+  //salva o ponteiro no arquivo 
+  void storeRegister(register* ptr) {
     
+    FILE *Register = fopen("Register.txt", "a");  
+    if (Register == NULL) {
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+    
+    fprintf(Register, "Nome: %s Email: %s Senha: %s Cpf: %s\n", ptr.name,     ptr.email, ptr.password, ptr.cpf);
+    if(option == 0) {
+      fprinf(Register, "Tipo: Residente");
+    }else {
+      fprintf(Register, "Tipo: Preceptor");
+    }
+    
+    fclose(Register);
   }
 
 int main(void) {
   
+  //ainda precisa criar a função que vai colocar os parametros em createProfile
+  createProfile();
+  
+  storeRegister(createProfile());
+  
+  freeProfile(ptr);
+
   return 0;
 }
