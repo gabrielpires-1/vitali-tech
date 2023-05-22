cf512f19720d51f566566c7b1148fa507d486469
TomCabral <apbc@cesar.school>
2023-05-22 10:30:49 -0300

    Update main.c
    Adicionada a função de deletar usuário.

--- main.c
+++ main.c
@@ -1,109 +1,125 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <windows.h>
 #include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
   char email[50];
   char password[50];
+  char newName[50], newEmail[50], newPassword[50], newCpf[12], newRole[50];
+  char delName[50], delEmail[50];
   system("color 0b");
   system("cls");
-  User * usr = malloc(sizeof(User));;
+  User *usr = malloc(sizeof(User));
   usr->name = malloc(51 * sizeof(char));
   usr->email = malloc(51 * sizeof(char));
   usr->password = malloc(51 * sizeof(char));
   usr->cpf = malloc(12 * sizeof(char));
   usr->role = malloc(51 * sizeof(char));
 
-  // use o código abaixo para criar um diretor, caso o txt seja apagado.
-  // storeRegister(createProfile("diretor","diretor@hospital.com", "senha123", "99999999999", "gestor"));
+  // use o código abaixo para criar um gestor, caso o txt seja apagado.
+  // storeRegister(createUser("diretor","diretor@hospital.com", "senha123",
+  // "99999999999", "gestor"));
 
-  int isLoggedIn = 0;
+  int isLoggedIn = 0, cpfAux = 0;
   do {
-  printf("\t\t\tSeja bem-vindo ao VitaliJourney! Seu hospital virtual!\n\nLogin (email):");
-  scanf("%s", email);
-  printf("Senha: ");
-  scanf("%s", password);
+    printf("\t\t\tSeja bem-vindo ao VitaliJourney! Seu hospital "
+           "virtual!\n\nLogin (email):");
+    scanf("%s", email);
+    printf("Senha: ");
+    scanf("%s", password);
 
-  if(isLoggedIn = login(email, password, usr)){
-    system("cls");
-		printf("\n\t\t\tBem vindo, %s!",usr->name);
-		printf("\n|Email:\t\t%s",usr->email);
-		printf("\n|CPF:\t\t%s",usr->cpf);
-		printf("\n|Cargo: \t%s", usr->role);
-    if(!strcmp(usr->role, "gestor")){
-      int choice;
+    if (isLoggedIn = login(email, password, usr)) {
+      system("cls");
+      printf("\n\t\t\tBem vindo, %s!", usr->name);
+      printf("\n|Email:\t\t%s", usr->email);
+      printf("\n|CPF:\t\t%s", usr->cpf);
+      printf("\n|Cargo: \t%s", usr->role);
+      if (!strcmp(usr->role, "gestor")) {
+        int choice;
       menu:
-      printf("\n\n\t\t\tO que voce gostaria de fazer hoje?\n");
-      printf("1 - Cadastrar um novo residente ou preceptor\n");
-      printf("2 - Apagar um residente ou preceptor do sistema\n");
-      printf("3 - Visualizar lista de residentes cadastrados no sistema\n");
-      printf("4 - Visualizar lista de preceptores cadastrados no sistema\n");
-      printf("5 - sair do programa\n");
-      scanf("%d", &choice);
-      User * newUser = malloc(sizeof(User));
-      switch (choice) {
+        printf("\n\n\t\t\tO que voce gostaria de fazer hoje?\n");
+        printf("1 - Cadastrar um novo residente ou preceptor\n");
+        printf("2 - Apagar um residente ou preceptor do sistema\n");
+        printf("3 - Visualizar lista de residentes cadastrados no sistema\n");
+        printf("4 - Visualizar lista de preceptores cadastrados no sistema\n");
+        printf("5 - sair do programa\n");
+        scanf("%d", &choice);
+        switch (choice) {
         case 1:
-          newUser->name = malloc(51 * sizeof(char));
-          newUser->email = malloc(51 * sizeof(char));
-          newUser->password = malloc(51 * sizeof(char));
-          newUser->cpf = malloc(12 * sizeof(char));
-          newUser->role = malloc(51 * sizeof(char));
-		      printf("\n\t\t\tVamos cadastrar um novo usuario!");
-          printf("Qual o nome do usuario?\n");
-          scanf("%s", newUser->name);
-          printf("Qual sera o email do usuario?\n");
-          scanf("%s", newUser->email);
-          printf("Qual sera a senha do usuario?\n");
-          scanf("%s", newUser->password);
-          printf("Qual eh o CPF do usuario?\n");
-          scanf("%s", newUser->cpf);
-          printf("Qual eh o cargo do usuario? (Digite \"preceptor\" ou \"residente)");
-          scanf("%s", newUser->role);
+          printf("\n\t\t\tVamos cadastrar um novo usuário!\n");
+          printf("Qual o nome do usuário?\n");
+          scanf("%s", newName);
+          printf("Qual será o email do usuário?\n");
+          scanf("%s", newEmail);
+          printf("Qual será a senha do usuário?\n");
+          scanf("%s", newPassword);
+          do {
+            printf("Qual é o CPF do usuário?\n");
+            scanf("%s", newCpf);
+            if (strlen(newCpf) != 11) {
+              printf("CPF inválido\n");
+              cpfAux = 1;
+            } else {
+              cpfAux = 0;
+            }
+          } while (cpfAux);
+          printf("Qual é o cargo do usuário? (Digite \"preceptor\" ou "
+                 "\"residente\")");
+          scanf("%s", newRole);
+
+          User *newUser =
+              createUser(newName, newEmail, newPassword, newCpf, newRole);
           storeRegister(newUser);
-          system("cls");
-          printf("Usuário cadastrado com sucesso no sistema!\n");
+          freeNewUser(newUser);
           goto menu;
           break;
+
         // DANILO E TOM VÃO IMPLEMENTAR ESSA FUNCIONALIDADE
-        case 2:
-          system("cls");
-		      printf("\n\t\t\tVamos apagar um usuario do sistema!",usr->name);
-          printf("Qual o nome do usuario?\n");
-          printf("Qual eh o email do usuario?\n");
-          printf("Qual eh o CPF do usuario?\n");
+        case 2: {
+          char nameToDelete[50];
+          char emailToDelete[50];
+
+          printf("\n\t\t\tVamos apagar um usuário do sistema!\n");
+          printf("Digite o nome do perfil a ser removido:\n");
+          scanf("%s", delName);
+          printf("Digite o e-mail do perfil a ser removido:\n");
+          scanf("%s", delEmail);
+
+          removeProfile(delName, delEmail);
+          goto menu;
           break;
+        }
         case 3:
           system("cls");
-		      list("residente");
+          list("residente");
           break;
         case 4:
           system("cls");
-		      list("preceptor");
+          list("preceptor");
           break;
         case 5:
           system("cls");
-          freeProfile(usr); // libera a memória alocada para encontrar o usuário
-		      exit(1);
+          freeUser(usr); // libera a memória alocada para encontrar o usuário
+          exit(1);
           break;
+        }
       }
-    }
       // caso o usuário seja um preceptor, esse bloco de código será executado.
-         else if (!strcmp(usr->role,"preceptor")) {
+      else if (!strcmp(usr->role, "preceptor")) {
         printf("\n\n\t\t\tO que voce gostaria de fazer hoje?\n");
         printf("1 - Aba de notas\n");
         printf("2 - Aba de feedbacks\n");
         printf("3 - Sair do programa\n");
       }
       // caso o usuário seja um residente, esse bloco de código será executado.
-      else if (!strcmp(usr->role,"residente")){
+      else if (!strcmp(usr->role, "residente")) {
         printf("\n\n\t\t\tO que voce gostaria de fazer hoje?\n");
         printf("1 - Aba de notas\n");
         printf("2 - Aba de feedbacks\n");
         printf("3 - Sair do programa\n");
-      } 
       }
+    }
   } while (isLoggedIn == 0);
   return 0;
 }
@@ -120,8 +136,8 @@ Nome, Email, senha, cpf, função (gestor, residente, preceptor)
 1 arquivo da biblioteca com as funcoes
 1 arquivo header
 um txt com os cadastros
-preceptores, residentes e gestores terão um cadastro(0 - residente, 1 - preceptor 2 - gestor)
-especificar no readme isso de cada um ter seu cadastro
+preceptores, residentes e gestores terão um cadastro(0 - residente, 1 -
+preceptor 2 - gestor) especificar no readme isso de cada um ter seu cadastro
 Formatação do TXT(exemplo):
 Nome: gabriel Email: email@email.com Senha: senha123 Cpf: 111111
 Tipo: Preceptor
@@ -160,13 +176,15 @@ estilo menu
       -perguntas norteadoras(responder de 0 a 5):
       Exemplo:
       -(0-5)o residente se mostrou capaz de se comunicar bem com seus colegas?
-      -(0-5)o residente conseguiu passar confiança dos conteudos aprendidos na faculdade?
+      -(0-5)o residente conseguiu passar confiança dos conteudos aprendidos na
+faculdade?
       -(0-5) outras perguntas (vamo ver ainda as perguntas)
       Tags:
       -deseja inserir alguma tag pra complementar a avaliação?
       Exemplos:
-      -o residente nao conseguiu se comunicar de forma eficiente com seus colegas
-      -o residente nao passou confiança dos conhecimentos aprendidos na faculdade
+      -o residente nao conseguiu se comunicar de forma eficiente com seus
+colegas -o residente nao passou confiança dos conhecimentos aprendidos na
+faculdade
 
     2- aba de feedbacks
       -exibir feedbacks do usuario
