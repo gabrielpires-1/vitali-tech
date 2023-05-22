63ba74896e4175ddd3c722cbf668ec441efcf10d
TomCabral <apbc@cesar.school>
2023-05-22 10:30:12 -0300

    Update lib.h
    Adicionada a função de apagar perfil.

--- lib.h
+++ lib.h
@@ -1,26 +1,31 @@
 #ifndef __LIB__
 #define __LIB__
 
-typedef struct User{
-  char * name;
-  char * email;
-  char * password;
-  char * cpf;
-  char * role; // residente, preceptor, gestor
-}User;
+typedef struct User {
+  char *name;
+  char *email;
+  char *password;
+  char *cpf;
+  char *role; // residente, preceptor, gestor
+} User;
 
-void list(char * role);
+void list(char *role);
 
 // função para realizar login - retorna 1 se login OK, retorna 0 se erro
-int login(char name[50], char password[50], User * usr);
+int login(char name[50], char password[50], User *usr);
 
-//insere um novo "usuário"
-User * createProfile(char *name, char *email, char *password, char *cpf, char *role);
+// insere um novo "usuário"
+User *createUser(char *newName, char *newEmail, char *newPassword, char *newCpf,
+                 char *newRole);
 
-//libera a memoria do ponteiro e libera da senha pq strdup() duplica a string
-void freeProfile(User* usr);
+void removeProfile(const char *name, const char *email);
 
-//salva o usuário no arquivo
-void storeRegister(User* usr);
+// libera a memoria do ponteiro do usuario atual
+void freeUser(User *usr);
+// libera a memoria do ponteiro do novo usuario
+void freeNewUser(User *newUser);
+
+// salva o usuário no arquivo
+void storeRegister(User *newUser);
+
 #endif //__LIB.H__
-
